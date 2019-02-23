651440b8e02cc7a991c50c512ce81f2edf88cf38
vppatel111 <vppatel@ualberta.ca>
2019-02-22 22:19:57 -0700

    The cd command works!

--- assignment2_main.rb
+++ assignment2_main.rb
@@ -7,6 +7,12 @@
 
 require_relative 'shell/shell'
 
-puts "hello shell!"
 shell = Shell.new
 shell.start
+
+# Setup steps:
+# Ensure you have swig installed.
+# To be able to run the "dp" command, do the following:
+#   1. Go into the precision directory.
+#   2. Run "ruby setup_precision.rb".
+#   3. You can now use dp from the shell. Ex) dp 2000 "Hello, world!"
--- shell/shell_commands.rb
+++ shell/shell_commands.rb
@@ -1,4 +1,19 @@
+require "test/unit"
+
 #Commands here will instantiate a command object then pass a block to it
 module ShellCommands
+  include Test::Unit::Assertions
+
+  def self.cd(filepath)
+
+    # assert filepath[0].length > 0 -> Assertions aren't working, not sure why.
+
+    begin
+      Dir.chdir(filepath[0]) # Slight bug, it breaks slightly when
+    rescue SystemCallError
+      puts "Target directory does not exist."
+    end
+
+  end
 
 end
