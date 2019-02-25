571cba9ed146ed4f9be692d4b41f0368d48927c8
curtis goud <cgoud@ualberta.ca>
2019-02-25 10:11:38 -0700

    assert command.rb

--- shell/command.rb
+++ shell/command.rb
@@ -6,21 +6,28 @@ class Command
 	attr_reader :block, :nonblock
 
 	def initialize(nonblock = false, &block)
+		#pre
+		assert ((nonblock == true) || (nonblock == false))
+		assert block.is_a? Proc
+
 		@nonblock = nonblock
 		@block = block
 
+		#post
+		assert ((@nonblock == true) || (@nonblock == false))
+		assert @block.is_a? Proc
 		assert valid?
 	end
 
 	def valid?
 		return false unless ((@nonblock == true) || (@nonblock == false))
 		return false unless @block.is_a? Proc
-
 		return true
 	end
 
 	def execute(*args)
 		# block passed in is the command itself?
+		#pre
 		assert valid?
 
 		thread = Thread.new do
@@ -28,6 +35,7 @@ class Command
 		end
 
 
+		#post
 		assert thread.alive?
 		assert valid?
 
@@ -35,17 +43,30 @@ class Command
 	end
 
 	def call(*args)
+		#pre
+		assert valid?
+
 		self.execute(*args)
+
+		#post
+		assert valid?
 	end
 
 	def wait(thread)
+		#pre
 		assert valid?
+		assert thread.is_a? Thread
+		assert thread.alive?
+
 		# waits until thread has finished calling
 		thread.join
+
+		#post
 		assert valid?
 	end
 
 	def nonblocking?
+		#pre
 		assert valid?
 
 		return @nonblock
@@ -55,13 +76,15 @@ end
 class ForkCommand < Command
 	def execute(*args)
 		# block passed in is the command itself?
+		# pre
 		assert valid?
 
 		pid = Process.fork do
 			@block.call(*args)
 		end
 
-		assert pid > 0 #negative pid means error occured
+		#post
+		assert pid >= 0 #negative pid means error occured
 		assert valid?
 
 		return pid
@@ -69,7 +92,14 @@ class ForkCommand < Command
 
 	def wait(pid)
 		# waits until forked process has finished executing
+		#pre
+		assert valid?
+		assert pid.is_a? Numeric
+		assert pid >= 0
+
 		Process.wait(pid)
+
+		#post
 		assert valid?
 	end
 end
