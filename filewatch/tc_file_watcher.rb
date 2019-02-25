4999fdbcf669d5bf079f915ed718b2a432837dfd
Jori Romans <jromans@ualberta.ca>
2019-02-24 21:18:13 -0700

    test_multiple_options implemented

--- filewatch/tc_file_watcher.rb
+++ filewatch/tc_file_watcher.rb
@@ -22,4 +22,48 @@ class TestArithmetic < Test::Unit::TestCase
 
   end
 
+  def test_multiple_options
+
+      # Multiple -fs
+      assert_raise ArgumentError do
+        file_watcher = FileWatcher.new(["-f", "file1", "-t", "3", "-c", "ls", "-f", "file1"])
+      end
+
+      # Multiple -t
+      assert_raise ArgumentError do
+        file_watcher = FileWatcher.new(["-f", "file1", "-t", "3", "-c", "ls", "-t", "0"])
+      end
+
+      # Multiple -ds
+      assert_raise ArgumentError do
+        file_watcher = FileWatcher.new(["-f", "file1", "-t", "3", "-d", "ls", "-d", "ls"])
+      end
+
+      # Multiple -cs
+      assert_raise ArgumentError do
+        file_watcher = FileWatcher.new(["-f", "file1", "-t", "3", "-c", "ls", "-c", "ls"])
+      end
+
+      # Multiple -cs
+      assert_raise ArgumentError do
+        file_watcher = FileWatcher.new(["-f", "file1", "-t", "3", "-a", "ls", "-a", "ls"])
+      end
+
+      # -da
+      assert_raise ArgumentError do
+        file_watcher = FileWatcher.new(["-f", "file1", "-t", "3", "-d", "ls", "-a", "ls"])
+      end
+
+      # -dc
+      assert_raise ArgumentError do
+        file_watcher = FileWatcher.new(["-f", "file1", "-t", "3", "-d", "ls", "-c", "ls"])
+      end
+
+      # -ac
+      assert_raise ArgumentError do
+        file_watcher = FileWatcher.new(["-f", "file1", "-t", "3", "-a", "ls", "-c", "ls"])
+      end
+
+  end
+
 end
